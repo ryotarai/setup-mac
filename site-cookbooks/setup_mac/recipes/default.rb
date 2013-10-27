038ad0b2c1c3d34089487b7e2a63d171a2cd0e42
Ryota Arai <ryota.arai@gmail.com>
2013-10-27 19:10:22 +0900

    Install vagrant.

--- site-cookbooks/setup_mac/recipes/default.rb
+++ site-cookbooks/setup_mac/recipes/default.rb
@@ -47,6 +47,12 @@ dmg_package 'SourceTree' do
   action :install
 end
 
+dmg_package 'Vagrant' do
+  source 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg'
+  type 'pkg'
+  action :install
+end
+
 {
   "http://iterm2.com/downloads/stable/iterm2_v1_0_0.zip" => "iTerm",
   "http://i.agilebits.com/dist/1P/mac4/1Password-4.0.2.zip" => "1Password 4",
